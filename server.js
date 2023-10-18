541fe25b4042d5f8e8c874e943369d0a9d7a73ba
marcelovicentegc <marcelovicentegc@gmail.com>
2023-10-18 20:22:45 -0300

    feat: save letters to db

--- server.js
+++ server.js
@@ -1,28 +1,40 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
-const { parse } = require("querystring");
+const { MongoClient } = require("mongodb");
 
-const server = http.createServer((req, res) => {
-  if (req.method === 'POST') {
-    let body = '';
-    req.on('data', chunk => {
-        body += chunk
+const server = http.createServer(async (req, res) => {
+  const client = await new MongoClient(process.env.MONGODB_URI).connect();
+
+  if (req.method === "POST") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk;
     });
-    req.on('end', () => {
-        const parsedBody = JSON.parse(body)
-        console.log("TODO: Save to DB")
-        console.log({ parsedBody })
-        res.end('OK');
+    req.on("end", () => {
+      try {
+        const parsedBody = JSON.parse(body);
+        client
+          .db(process.env.MONGODB_DB)
+          .collection("letters")
+          .insertOne(parsedBody);
+
+        res.statusCode = 201;
+        res.end("Ok");
+      } catch (error) {
+        console.log(error);
+        res.statusCode = 500;
+        res.end("Internal Server Error");
+      }
     });
 
     res.setHeader("Content-Type", "application/json");
-    res.statusCode = 201;
-    return
-  } 
+    return;
+  }
 
   // Parse the request URL to determine which file to serve
-  const url = req.url === "/" || req.url.includes("/?enviado") ? "/index.html" : req.url;
+  const url =
+    req.url === "/" || req.url.includes("/?enviado") ? "/index.html" : req.url;
   const filePath = path.join(__dirname, url);
 
   // Check if the file exists
